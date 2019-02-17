594733d0701626236f7dd4733c95937570d0ae5c
sergiojerezrivera <dawsergiojerez@gmail.com>
2019-02-17 17:07:08 +0000

    using POSTMAN as a Rest Client for testing URL params and methods GET & POST on HTTP requests

--- app.js
+++ app.js
@@ -17,7 +17,20 @@ app.use(bodyParser.json());
 
 
 //Rutas
+app.get('/', (req, res) => {
+    res.status(200).send(
+        "<h1>Pagina de inicio</h1>"
+    );
+}); 
 
+app.post('/test/:id', (req, res) => {
+    console.log(req.body.nombre);
+    console.log(req.query.web);
+    console.log(req.params.id);
+    res.status(200).send({
+        message: "Hola Mundo desde mi API de NodeJS"
+    });
+}); 
 
 //Exportar
 module.exports = app;
