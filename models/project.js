4711d3fe69c74e543598807b177db99765282323
sergiojerezrivera <dawsergiojerez@gmail.com>
2019-02-17 17:22:19 +0000

    Creation of a model for projects with properties and exporting the module to save it into the collection

---
+++ models/project.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProjectSchema = Schema({
+    name: String,
+    description: String,
+    category: String,
+    year: Number,
+    langs: [String]
+});
+
+//mongoose guarda el nombre de la entidad 'Project' quitando la 
+//mayuscula y agregando el plural a la palabra result: 'projects'
+module.exports = mongoose.model('Project', ProjectSchema);
