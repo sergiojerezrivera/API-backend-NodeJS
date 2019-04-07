8896590689739be49c66ef5dadad197ac433848e
sergiojerezrivera <dawsergiojerez@gmail.com>
2019-04-07 19:18:50 +0100

    Ready to upload it to GITHUB

--- app.js
+++ app.js
@@ -35,23 +35,7 @@ app.use('/api', project_routes);
 
 
 
-
-
-//Rutas de prueba
-// app.get('/', (req, res) => {
-//     res.status(200).send(
-//         "<h1>Pagina de inicio</h1>"
-//     );
-// }); 
-
-// app.post('/test/:id', (req, res) => {
-//     console.log(req.body.nombre);
-//     console.log(req.query.web);
-//     console.log(req.params.id);
-//     res.status(200).send({
-//         message: "Hola Mundo desde mi API de NodeJS"
-//     });
-// }); 
+ 
 
 //Exportar
 module.exports = app;
